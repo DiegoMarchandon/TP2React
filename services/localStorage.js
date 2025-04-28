d4f7ab3811b1e0425c0872413b7882fe13e378f1
DiegoMarchandon <diegomarc2020@gmail.com>
2025-04-28 00:30:22 -0300

    localStorage

---
+++ services/localStorage.js
@@ -0,0 +1,18 @@
+const lenguaje = {
+    lenguajePref: "es"
+};
+
+const favoritos = {
+    // formato de ejemplo: 
+    /* [
+        fechaID: " ", //usamos la fecha como ID porque es la foto DEL dia, y no se repite
+        titulo: " ",
+        url:" ", //como la URL en HD no está en todas las imagenes, usamos la común. O en su defecto, un operador lógico para indicar que si no está la imagen en HD, usemos la otra.
+
+    ],
+    [...]
+    */
+};
+
+localStorage.setItem("language",JSON.stringify(lenguaje));
+localStorage.setItem("favoritos",JSON.stringify(favoritos));
