0a14f588cce59b40c8584c30ee8519dd55075863
DiegoMarchandon <diegomarc2020@gmail.com>
2025-05-01 23:05:36 -0300

    agregando mensaje en home + estilos para msj de Error 400

--- pages/Home/Home.jsx
+++ pages/Home/Home.jsx
@@ -9,6 +9,7 @@ function Home() {
     const [clave, setClave] = useState(0);
     const [apod, setApod] = useState([]);
     const [filtroApod, setFiltroApod] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
 
     const refrescarHijo = () => {
         setClave(clave + 1);
@@ -42,10 +43,15 @@ function Home() {
                 const response = await fetch(
                     `https://api.nasa.gov/planetary/apod?api_key=G57yQ8LZkubQWfqf9tKr81jGNhi3CsM39XtIJiQu&date=${selectedDate}`
                 );
+                if (!response.ok) {
+                    throw new Error(`Error ${response.status}: La fecha ingresada no es válida.`);
+                }            
                 const data = await response.json();
                 setApod([data]); //lo pongo como array para que funcione el putoo map
+                setErrorMessage(""); //limpiamos el mensaje de error cuando la solicitud es exitosa
             }catch(error){
-                console.error("Error fetching data:", error);
+                // console.error("Error fetching data:", error);
+                setErrorMessage(error.message); // Guardo el mensaje de error en el estado
             }
         }
     };
@@ -58,11 +64,13 @@ function Home() {
     return (
         <div className="min-h-screen flex flex-col bg-radial-[at_50%_75%] from-sky-200 via-blue-300 to-indigo-900 to-90%">
             <Header key={clave} />
+            <h1 className="text-5xl font-extrabold text-blue-100 mb-4 drop-shadow-lg text-center">{t('welcome')}</h1>
+            <h3 className="text-xl mb-8 text-center drop-shadow-md">{t('home.instruction')}</h3>
             <div className='flex flex-col items-center justify-center w-full gap-4'>
+                {errorMessage && <p className="text-red-500 font-semibold">{errorMessage}</p>}
                 <input type="date" value={filtroApod} onChange={handleChange} className=" bg-white w-40 rounded-sm w-xs size-8 opacity-70" />
                 <button className='w-34 mt-1 mb-4 bg-red-500 hover:bg-red-600 text-white font-semibold py-2 px-4 rounded-lg transition duration-300 ease-in-out transform hover:-translate-y-1 cursor-pointer' onClick={handleReset}>{t('home.boton')}</button>
             </div>
-            <h1>{t('home.description')}</h1>
             <div className="flex-grow flex flex-wrap gap-4 p-4 justify-center">
                 {apod.map((item) => (
                     <Card
