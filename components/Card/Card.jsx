e01d8498dc4c3851c8a7bd414b327dccd84c0cbe
Ariel-Mamani <arielmamani764@gmail.com>
2025-05-05 22:44:24 -0300

    detalles esteticos

--- components/Card/Card.jsx
+++ components/Card/Card.jsx
@@ -11,7 +11,7 @@ const Card = ({ imageUrl, title, date, itemRef, refrescar }) => {
     return (
         <div className="relative w-[300px] min-h-[120px] bg-[#1b263bff] rounded-lg p-0 mb-2 flex flex-col justify-between items-start shadow-md transition-all duration-300 ease-in-out transform translate-y-0 hover:translate-y-[-4px] hover:shadow-lg cursor-pointer overflow-hidden">
             <button onClick={() => toggleFavorito(itemRef, refrescar)} className={`absolute left-63 top-2 ${estaEnFavoritos ? 'text-green-500' : 'text-blue-500'} hover:text-red-500 transition-colors duration-300`}><HeartPlus size={35} strokeWidth={2} /></button>
-            <img src={imageUrl} alt={title} className="w-full h-45 object-cover " />
+            <img src={imageUrl} alt={title}  className="w-full h-45 object-cover " />
             <div className="w-full p-4">
                 <h3 className="text-white text-center  text-lg ">{title}</h3>
                 <p className="text-white text-lg text-center">{date}</p>
--- src/App.jsx
+++ src/App.jsx
@@ -12,23 +12,15 @@ import { FavoritosProvider } from './context/favsContext';
 function App() {
 
   const { t, i18n } = useTranslation();
-
   const changeLanguage = (language) => {
     i18n.changeLanguage(language); // Cambia el idioma
     localStorage.setItem('language', language); // Guarda la preferencia en localStorage
   };
 
-
   return (
     <Router>
       <FavoritosProvider>
       <div className="App">
-        {/* Botones para cambiar el idioma */}
-        {/* <div style={{ padding: '10px', textAlign: 'center' }}>
-          <button onClick={() => changeLanguage('es')}>{t('language')} Español</button>
-          <button onClick={() => changeLanguage('en')}>{t('language')} English</button>
-        </div> */}
-        {/* Contenido de la aplicación */}
         <Routes>
           {/* en Element y entre llaves van los components de cada pagina que creemos. Ej {<Home/>} o {<Contacto/>} */}
           <Route path={ROUTES.home} element={<Home/>} />
