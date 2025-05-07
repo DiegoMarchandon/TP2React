78a7125e6caa0c7f7526828a49e19b50e048c137
DiegoMarchandon <diegomarc2020@gmail.com>
2025-05-07 20:10:29 -0300

    agregando ruta base

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: './',
   server: {
     historyApiFallback: true //si el servidor de desarrollo no encuentra una ruta física, sirva el index.html index igual
   }
